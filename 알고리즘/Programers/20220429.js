7b3f44ba0f4cbf9ae87dfc3c8f8977ce54969b51
Dabin Kim <dabin1004i@naver.com>
2022-04-29 13:59:14 +0900

    Update 신규 아이디 추천

--- 알고리즘/Programers/20220429.js
+++ 알고리즘/Programers/20220429.js
@@ -1,19 +1,34 @@
 //숫자 문자열과 영단어
 function solution(s) {
-    const numberText = [
-        "zero",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine"
-    ]
-    for (let i = 0; i < 10; i++) {
-        s = s.split(numberText[i]).join(i);
-    }
-    return Number(s);
+  const numberText = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  for (let i = 0; i < 10; i++) {
+    s = s.split(numberText[i]).join(i);
+  }
+  return Number(s);
+}
+
+//신규 아이디 추천
+function solution(new_id) {
+  const answer = new_id
+    .toLowerCase()
+    .replace(/[\{\}\[\]\/?,;:|\)*~`!^\+<>@\#$%&\\\=\(\'\"]/gi, "")
+    .replace(/\.{2,}/g, ".")
+    .replace(/^\.|\.$/g, "")
+    .replace(/^$/, "a")
+    .slice(0, 15)
+    .replace(/\.$/, "");
+  const len = answer.length;
+  //7단계 2자 이하 마지막문자를 길이 3이 될때까지 반복
+  return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
 }
