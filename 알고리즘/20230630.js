619ee6af5ef04faa60bb52eaecde87523019d0f4
Dabin Kim <dabin1004i@naver.com>
2023-07-02 21:54:13 +0900

    Update

--- 알고리즘/20230630.js
+++ 알고리즘/20230630.js
@@ -1,6 +1,46 @@
 //문자 반복 출력하기
 function solution(my_string, n) {
-  my_string = my_string.split("");
-  for (let i = 0; i < my_string.length - 1; i++) {}
+  let answer = "";
+  for (s of my_string.split("")) {
+    answer += s.repeat(n);
+  }
   return answer;
 }
+//맵으로 하는거
+//function solution(my_string, n) {
+//    var answer = [...my_string].map(v => v.repeat(n)).join("");
+//    console.log(answer);
+//    return answer;
+//}
+
+//최댓값 만들기 (1)
+function solution(numbers) {
+  numbers.sort((a, b) => b - a);
+  return numbers[0] * numbers[1];
+}
+
+//분수의 덧셈
+//기약분수 : 분모와 분자의 공약수가 1뿐인 분수 => 안나뉘는 분수
+
+//배열 두배 만들기
+function solution(numbers) {
+  let answer = [];
+  for (num of numbers) {
+    answer.push(num * 2);
+  }
+  return answer;
+}
+//reduce.......
+//function solution(numbers) {
+//  return numbers.reduce((a, b) => [...a, b * 2], []);
+//}
+
+//최빈값 구하기 (가장 많이 나오는 값)
+//function solution(array) {
+//  let answer = [];
+//  for (let i = 0; i < array.length; i++) {
+//    answer.push(array.filter((ea) => array[i] == ea).length);
+//  }
+//  answer.sort((a, b) => b - a);
+//  return answer;
+//}
