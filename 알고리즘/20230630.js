bc03bab340e38664e6d18b5436bd98d2bbc3bc5a
dbkim <dabin1004i@naver.com>
2023-07-03 17:27:11 +0900

    Update

--- 알고리즘/20230630.js
+++ 알고리즘/20230630.js
@@ -18,10 +18,6 @@ function solution(numbers) {
   numbers.sort((a, b) => b - a);
   return numbers[0] * numbers[1];
 }
-
-//분수의 덧셈
-//기약분수 : 분모와 분자의 공약수가 1뿐인 분수 => 안나뉘는 분수
-
 //배열 두배 만들기
 function solution(numbers) {
   let answer = [];
@@ -33,14 +29,4 @@ function solution(numbers) {
 //reduce.......
 //function solution(numbers) {
 //  return numbers.reduce((a, b) => [...a, b * 2], []);
-//}
-
-//최빈값 구하기 (가장 많이 나오는 값)
-//function solution(array) {
-//  let answer = [];
-//  for (let i = 0; i < array.length; i++) {
-//    answer.push(array.filter((ea) => array[i] == ea).length);
-//  }
-//  answer.sort((a, b) => b - a);
-//  return answer;
 //}
