0f6b45a21bd4dc4f418d500af260c1d824859cad
dbkim <dabin1004i@naver.com>
2023-07-10 17:55:48 +0900

    Update

--- 알고리즘/20230710.js
+++ 알고리즘/20230710.js
@@ -46,3 +46,50 @@ function solution(balls, share) {
 // Math.round(팩토리얼(balls) / 팩토리얼(balls - share) / 팩토리얼(share) )
 
 //인덱스 바꾸기
+// 그새 까먹어버린 splice
+function solution(my_string, num1, num2) {
+  let strings = my_string.split("");
+  let n1 = [...strings][num1];
+  strings.splice(num1, 1, strings[num2]);
+  strings.splice(num2, 1, n1);
+  return strings.join("");
+}
+//개뿔 구조분해할당으로 해결해버린다!
+//function solution(my_string, num1, num2) {
+//  my_string = my_string.split("");
+//  [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+//  return my_string.join("");
+//}
+
+//369게임
+function solution(order) {
+    return order.toString().split("").filter( i => ["3", "6", "9"].includes(i) ).length;
+}
+//matchAll(/[3|6|9]/g), new Set([3,6,9]);
+
+//약수 구하기
+function solution(n) {
+  var answer = [];
+  for (let i = 1; i <= n; i++) {
+    n % i == 0 ? answer.push(i !== 1 || i !== n ? n / i : 1) : 1;
+  }
+  return answer.sort((a, b) => a - b);
+}
+// push에 그냥 i 하면 됨
+
+//숫자 찾기
+//function solution(num, k) {
+//  let counter = num.toString().indexOf(k);
+//  return counter > 0 ? counter + 1 : -1;
+//}
+function solution(num, k) {
+    return num.toString().indexOf(k)+1 || -1;
+}
+
+//문자열 정렬하기 (2)
+function solution(my_string) {
+  return my_string.toLowerCase().split("").sort().join("");
+}
+//    return [...s.toLowerCase()].sort().join('')
+
+//합성수 찾기
