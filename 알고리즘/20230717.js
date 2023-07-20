69afb324b0e02d74ddec304839f20c155f937e45
dbkim <dabin1004i@naver.com>
2023-07-20 16:23:21 +0900

    Update

--- 알고리즘/20230717.js
+++ 알고리즘/20230717.js
@@ -1,5 +1,5 @@
-//소인수분해
-function solution(n) {
+  //소인수분해
+  function solution(n) {
   var answer = [];
   let checker = 2;
   while (n >= 2) {
@@ -11,47 +11,47 @@ function solution(n) {
     }
   }
   return [...new Set(answer)];
-}
-/// new Set() 은 중복값을 제거하기 때문에 중요!!!
+  }
+  /// new Set() 은 중복값을 제거하기 때문에 중요!!!
 
-//컨트롤 제트
-function solution(s) {
+  //컨트롤 제트
+  function solution(s) {
     let arr = s.split(" ");
     while (arr.indexOf("Z") != -1) {
         arr.splice(arr.indexOf("Z")-1, 2);
     } 
     return arr.length != 0 ? Number(arr.reduce( (sum, val) => Number(sum) + Number(val) )) : 0;
-}
+  }
 
-//중복된 문자 제거
-function solution(my_string) {
+  //중복된 문자 제거
+  function solution(my_string) {
   return [...new Set(my_string.split(""))].join("");
-}
-///생성자 함수 또한 바로 Spread 연산자 가능
-//return [...new Set(my_string)].join('');
+  }
+  ///생성자 함수 또한 바로 Spread 연산자 가능
+  //return [...new Set(my_string)].join('');
 
-//A로 B 만들기
-function solution(before, after) {
+  //A로 B 만들기
+  function solution(before, after) {
   return [...before].sort().join("") == [...after].sort().join("") ? 1 : 0;
-}
+  }
 
-//k의 개수
-function solution(i, j, k) {
+  //k의 개수
+  function solution(i, j, k) {
     let counter = 0;
     while(i <= j ) {
         counter += i.toString().split('').filter((word) => word == k).length;
         i++;
     }
     return counter;
-}
-// split 으로 나눠가지고 하는 방법도있음
-//let a = "";
-//for (i; i <= j; i++) { a += i; }
-//return a.split(k).length - 1;
+  }
+  // split 으로 나눠가지고 하는 방법도있음
+  //let a = "";
+  //for (i; i <= j; i++) { a += i; }
+  //return a.split(k).length - 1;
 
-//직사각형 넓이 구하기
-//Math.sqrt 루트 Math.pow(숫자, 배수)
-function solution(dots) {
+  //직사각형 넓이 구하기
+  //Math.sqrt 루트 Math.pow(숫자, 배수)
+  function solution(dots) {
     dots.sort();
     return Math.abs((dots[dots.length-1][0]-dots[0][0]) * (dots[dots.length-1][1]-dots[0][1]));
-}
+  }
--- 알고리즘/20230719.js
+++ 알고리즘/20230719.js
@@ -1,1 +1,33 @@
 //한 번만 등장한 문자
+function solution(s) {
+  s = s.split("").sort().join("");
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      s = s.replaceAll(s[i], "");
+      i--;
+    }
+  }
+  return s;
+}
+// 와우 미친 맨 뒤에서부터 앞으로;;;
+//function solution(s) {
+//  let res = [];
+//  for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
+//  return res.sort().join("");
+//}
+// 이건 또 뭐야
+//var solution=s=>[...s].filter(c=>s.match(new RegExp(c,'g')).length==1).sort().join('')
+
+//문자열 계산하기
+function solution(my_string) {
+  return eval(my_string.replaceAll(" ", ""));
+}
+
+//숨어있는 숫자의 덧셈 (2)
+function solution(my_string) {
+    return my_string.replace(/[^0-9]/g, "*").split("*").reduce((sum, val)=> Number(sum) + Number(val), 0)
+}
+// 초기값이 0으로 지정되어야만 통과하는 이유를 확인해봐야하는듯?
+// 정규식 /\D+/ 을 사용해도됨.
+
+//이진수 더하기
