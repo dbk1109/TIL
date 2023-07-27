dcce21e41581e2a5440e1042fb77281730df2172
dbkim <dabin1004i@naver.com>
2023-07-27 14:43:39 +0900

    Update 유한소수 판별하기

--- 알고리즘/20230727.js
+++ 알고리즘/20230727.js
@@ -1,7 +1,12 @@
 //유한소수 판별하기
+//  기약분수로 나타내었을 때 분모의 소인수가 2와 5만 존재해야함.
 function solution(a, b) {
-  function checker(n) {
     var answer = [];
+    let gcd = 1;
+    for(let i=2; i <= Math.min(a, b); i++){
+      if(a % i === 0 && b % i === 0) gcd = i;
+    }
+    let n = b / gcd;
     let checker = 2;
     while (n >= 2) {
       if (n % checker === 0) {
@@ -11,17 +16,10 @@ function solution(a, b) {
         checker++;
       }
     }
-    return [...new Set(answer)];
-  }
-  let gcd = 1;
-  for (let i = 2; i <= Math.min(a, b); i++) {
-    if (a % i === 0 && b % i === 0) {
-      gcd = i;
-    }
-  }
-  if (checker(b / gcd).length > 1) {
-    return checker(b / gcd).includes(2) && checker(b / gcd).includes(5) ? 1 : 2;
-  } else {
-    return checker(b / gcd).includes(2) || checker(b / gcd).includes(5) ? 1 : 2;
-  }
+    let test = [...new Set(answer)].filter( el => ![2, 5].includes(el) );
+    return test.length > 0 ? 2 : 1;
 }
+/// 사실 이렇게도됨ㅋㅋㅋ
+//function solution(a, b) {
+//  return (a / b).toString().length > 15 ? 2 : 1;
+//}
