a97c8ce212ccbf34f547c32805f60dd4d662c4fe
dbkim <dabin1004i@naver.com>
2023-07-27 09:59:05 +0900

    Update

--- 알고리즘/20230726.js
+++ 알고리즘/20230726.js
@@ -70,31 +70,3 @@ function solution(dots) {
 //    if((dots[0][1] - dots[3][1]) / (dots[0][0] - dots[3][0]) === (dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0])) return 1
 //    return 0
 //}
-
-//유한소수 판별하기
-function solution(a, b) {
-  function checker(n) {
-    var answer = [];
-    let checker = 2;
-    while (n >= 2) {
-      if (n % checker === 0) {
-        n = n / checker;
-        answer.push(checker);
-      } else {
-        checker++;
-      }
-    }
-    return [...new Set(answer)];
-  }
-  let gcd = 1;
-  for (let i = 2; i <= Math.min(a, b); i++) {
-    if (a % i === 0 && b % i === 0) {
-      gcd = i;
-    }
-  }
-  if (checker(b / gcd).length > 1) {
-    return checker(b / gcd).includes(2) && checker(b / gcd).includes(5) ? 1 : 2;
-  } else {
-    return checker(b / gcd).includes(2) || checker(b / gcd).includes(5) ? 1 : 2;
-  }
-}
