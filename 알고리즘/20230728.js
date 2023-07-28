b8b5aca65025eee1e414f597daed62c4e4a1afd2
dbkim <dabin1004i@naver.com>
2023-07-28 15:11:43 +0900

    Update 등수 매기기

--- 알고리즘/20230728.js
+++ 알고리즘/20230728.js
@@ -1,1 +1,24 @@
 //등수 매기기
+function solution(score) {
+    let arr = [];
+    let nexter = 1;
+    score.map(x => arr.push(((x[0] + x[1]) / 2)));
+    let rank = [...arr].sort((a,b) => b-a);
+    function replacer(array, target, ranks) {
+        for(i in array) {
+            if(array[i] === target) array[i] = ranks.toString();
+        }
+    }
+    for (let i = 0; i < score.length; i++, nexter++) {
+        replacer(arr, rank[i], nexter);
+    }
+    return arr.map(x => Number(x));
+}
+// 미친
+//function solution(score) {
+//  return score.map((el) => {
+//    return (
+//      score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+//    );
+//  });
+//}
