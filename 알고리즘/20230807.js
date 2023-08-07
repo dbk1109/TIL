bbfe558a1cff415d66b40ae0c8fe784fbc8164f5
dbkim <dabin1004i@naver.com>
2023-08-07 11:34:44 +0900

    Update 문자 리스트를 문자열로 변환하기

--- 알고리즘/20230807.js
+++ 알고리즘/20230807.js
@@ -11,3 +11,7 @@ function solution(str1, str2) {
 //  return [...str1].map((x, idx)=> x+str2[idx]).join("");
 //  객체지향 예제도 있으나 너무 길어서 생략함. class와 constructor를 이용해 구현함
 
+//문자 리스트를 문자열로 변환하기
+function solution(arr) {
+  return arr.join("");
+}
