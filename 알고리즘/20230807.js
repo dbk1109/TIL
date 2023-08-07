e8d9255cf74c5c1e7bf60660f72071e1d7e10ef4
dbkim <dabin1004i@naver.com>
2023-08-07 11:33:49 +0900

    Update 문자열 섞기

---
+++ 알고리즘/20230807.js
@@ -0,0 +1,13 @@
+//문자열 섞기
+function solution(str1, str2) {
+  let answer = "";
+  for (let i = 0; i < str1.length; i++) {
+    answer += str1[i];
+    answer += str2[i];
+  }
+  return answer;
+}
+//  return [...str1].reduce((prev, char, i) => prev + char + str2[i], '');
+//  return [...str1].map((x, idx)=> x+str2[idx]).join("");
+//  객체지향 예제도 있으나 너무 길어서 생략함. class와 constructor를 이용해 구현함
+
