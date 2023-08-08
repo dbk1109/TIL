4fba717747747e9a8fe6528f46eaf17bd7d94146
dbkim <dabin1004i@naver.com>
2023-08-08 14:44:56 +0900

    Update 등차수열의 특정한 항만 더하기

--- 알고리즘/20230808.js
+++ 알고리즘/20230808.js
@@ -11,3 +11,13 @@ function solution(code) {
     })
     return ret.length > 0 ? ret : "EMPTY";
  }
+
+ //등차수열의 특정한 항만 더하기
+function solution(a, d, included) {
+    return included.map( (x, idx) => x ? a + (d * idx) : 0 ).reduce((sum, val) => sum + val)
+}
+//  reduce만 쓰는 방법
+//    return included.reduce((acc, flag, i) => {
+//      return flag ? acc + a + d * i : acc;
+//    }, 0);
+
