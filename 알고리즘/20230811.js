66e463056f98db373dc2a50d7e6635b5063802ea
dbkim <dabin1004i@naver.com>
2023-08-11 14:40:39 +0900

    Update 콜라츠 수열 만들기

--- 알고리즘/20230811.js
+++ 알고리즘/20230811.js
@@ -19,3 +19,20 @@ function solution(start, end) {
 //  array가 아니면 만들면 될 일!
 //  return Array(end-start+1).fill(start).map((x,idx) => x+idx);
 
+//콜라츠 수열 만들기
+function solution(n) {
+  var answer = [];
+  while (n != 1) {
+    answer.push(n);
+    n % 2 === 0 ? (n = n / 2) : (n = 3 * n + 1);
+  }
+  answer.push(1);
+  return answer;
+}
+//  재귀는 늘 어렵군
+//function solution(n, arr = []) {
+//  arr.push(n);
+//  if (n === 1) return arr;
+//  if (n % 2 === 0) return solution(n / 2, arr);
+//  return solution(3 * n + 1, arr);
+//}
