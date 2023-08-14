20dfa8693ce3c78f9153b32adcd6c521ff5fdfe6
dbkim <dabin1004i@naver.com>
2023-08-14 15:10:31 +0900

    Update 문자열 여러 번 뒤집기

--- 알고리즘/20230814.js
+++ 알고리즘/20230814.js
@@ -4,3 +4,20 @@ function solution(x1, x2, x3, x4) {
 }
 //  첨에 뭔말인지 이해못했잖어; 논리합 OR ∨ , 논리곱 AND ∧
 
+//문자열 여러 번 뒤집기
+function solution(my_string, queries) {
+  for ([s, e] of queries) {
+      my_string = my_string.split('');
+      my_string = my_string.slice(0,s).join('') + my_string.slice(s,e+1).reverse().join('') +  my_string.slice(e+1).join('')
+  }
+  return my_string;
+}
+//  깔끔해...
+//function solution(my_string, queries) {
+//  let str = my_string.split("");
+//  queries.forEach(([start, end]) => {
+//    const changeStr = str.slice(start, end + 1);
+//    str.splice(start, changeStr.length, ...changeStr.reverse());
+//  });
+//  return str.join("");
+//}
