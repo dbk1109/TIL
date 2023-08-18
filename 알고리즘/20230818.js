9902502daf9fc6ece560b3e8da1f308823dadf0b
dbkim <dabin1004i@naver.com>
2023-08-18 17:41:28 +0900

    Update 특정한 문자를 대문자로 바꾸기

--- 알고리즘/20230818.js
+++ 알고리즘/20230818.js
@@ -41,3 +41,8 @@ function solution(my_strings, parts) {
 function solution(intStrs, k, s, l) {
   return intStrs.map((el) => Number(el.slice(s, s + l))).filter((x) => x > k);
 }
+
+//특정한 문자를 대문자로 바꾸기
+function solution(my_string, alp) {
+  return my_string.replaceAll(alp, alp.toUpperCase());
+}
