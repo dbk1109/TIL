0c7201d46da79e68f39dd7217b4563193037bbb6
dbkim <dabin1004i@naver.com>
2023-08-18 17:15:56 +0900

    Update 접미사 배열

--- 알고리즘/20230818.js
+++ 알고리즘/20230818.js
@@ -15,3 +15,10 @@ function solution(a, b, c, d) {
 }
 
 //접미사 배열
+function solution(my_string) {
+  let answer = [];
+  for (let i = 0; i < my_string.length; i++) {
+    answer.push(my_string.slice(i, my_string.length));
+  }
+  return answer.sort();
+}
