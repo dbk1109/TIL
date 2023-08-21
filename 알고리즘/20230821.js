23b7258ad7de20d8256a4734ea6dd75d2baa5cb4
dbkim <dabin1004i@naver.com>
2023-08-21 18:43:57 +0900

    Update 접두사인지 확인하기

--- 알고리즘/20230821.js
+++ 알고리즘/20230821.js
@@ -2,3 +2,12 @@
 function solution(my_string, n) {
   return my_string.slice(0, n);
 }
+
+//접두사인지 확인하기
+function solution(my_string, is_prefix) {
+  var answer = [];
+  for (let i = 1; i < my_string.length; i++) {
+    answer.push(my_string.slice(0, i));
+  }
+  return answer.includes(is_prefix) ? 1 : 0;
+}
