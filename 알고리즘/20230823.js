d3e01b54cddb5621fa21dcf9382dc1e036dbfbce
dbkim <dabin1004i@naver.com>
2023-08-23 17:37:27 +0900

    Update 뒤에서 5등까지

--- 알고리즘/20230823.js
+++ 알고리즘/20230823.js
@@ -115,3 +115,8 @@ function solution(ms, pat) {
 function solution(num_list) {
   return num_list.sort((a, b) => a - b).slice(5);
 }
+
+//뒤에서 5등까지
+function solution(num_list) {
+  return num_list.sort((a, b) => a - b).slice(0, 5);
+}
