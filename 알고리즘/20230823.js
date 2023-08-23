62af7196c3d5ecb299f4fa9e97214f307a197d91
dbkim <dabin1004i@naver.com>
2023-08-23 14:31:28 +0900

    Update 홀수 vs 짝수

--- 알고리즘/20230823.js
+++ 알고리즘/20230823.js
@@ -7,3 +7,20 @@ function solution(str_list) {
   return [];
 }
 //  너무 복잡하게 생각하지 않아도 된다!
+
+//홀수 vs 짝수
+function solution(num_list) {
+    let CHECKER = [[],[]];
+    num_list.map((x, i) => i%2 === 0 ? CHECKER[1].push(x) : CHECKER[0].push(x))
+    let ODD = CHECKER[0].reduce((sum, val) => sum + val)
+    let EVEN = CHECKER[1].reduce((sum, val) => sum + val)
+    return ODD >= EVEN ? ODD : EVEN;
+}
+// 훨씬 간단한걸..?
+//function solution(num_list) {
+//  let [even, odd] = [0, 0];
+//  num_list.map((v, idx) => {
+//    !(idx % 2) ? (even += v) : (odd += v);
+//  });
+//  return odd > even ? odd : even;
+//}
