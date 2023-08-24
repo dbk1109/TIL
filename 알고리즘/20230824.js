91a28129270dfd8b82f878e687a7cf480f335b05
dbkim <dabin1004i@naver.com>
2023-08-24 10:53:57 +0900

    Update 문자열 잘라서 정렬하기

--- 알고리즘/20230824.js
+++ 알고리즘/20230824.js
@@ -2,3 +2,10 @@
 function solution(myString) {
   return myString.split("x").map((x) => x.length);
 }
+
+//문자열 잘라서 정렬하기
+function solution(myString) {
+    return myString.split('x').filter(x => x != "").sort();
+}
+//  정규식 매치를 잘 쓰는것도 중요하다
+//  const solution = (s) => s.match(/[^x]+/g).sort();
