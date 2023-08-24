163cc256378f50dbedb0a4dad5396344bcd1e666
dbkim <dabin1004i@naver.com>
2023-08-24 12:10:35 +0900

    Update 간단한 식 계산하기

--- 알고리즘/20230824.js
+++ 알고리즘/20230824.js
@@ -9,3 +9,17 @@ function solution(myString) {
 }
 //  정규식 매치를 잘 쓰는것도 중요하다
 //  const solution = (s) => s.match(/[^x]+/g).sort();
+
+//간단한 식 계산하기
+function solution(binomial) {
+    let [a, op, b] = binomial.split(" ").map((x, i) => i != 1 ? Number(x) : x);
+    switch(op) {
+        case "+" : return a + b;
+        case "-" : return a - b;
+        case "*" : return a * b;
+    }
+}
+//  아니이게뭐야
+//function solution(binomial) {
+//    return Function(`return ${binomial}`)()
+//}
