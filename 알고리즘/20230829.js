aa5ae1a85f5d71a917fd160220c4b3badda7ea8c
dbkim <dabin1004i@naver.com>
2023-08-29 14:37:42 +0900

    Update 정사각형으로 만들기

--- 알고리즘/20230829.js
+++ 알고리즘/20230829.js
@@ -2,3 +2,18 @@
 const solution = (a, b) => (BigInt(a) + BigInt(b)).toString();
 //  긴 숫자 노출방법 BigInt()
 //  String(BigInt(a) + BigInt(b)) 이렇게도 됨.
+
+//정사각형으로 만들기
+function solution(arr) {
+  let row    = arr.length;
+  let column = arr[0].length;
+  if (row > column) {
+      arr.forEach( function(a) { while(a.length < row) {a.push(0)} }) 
+  } else {
+      while(row < column-1) {
+          row = arr.length;
+          arr.push(new Array(column).fill(0))
+      }
+  }
+  return arr;
+}
