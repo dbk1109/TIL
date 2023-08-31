1412893eef9dc2b79d75576a6c8f5232d128b123
dbkim <dabin1004i@naver.com>
2023-08-31 17:08:01 +0900

    Update 약수의 개수와 덧셈

--- 알고리즘/20230831.js
+++ 알고리즘/20230831.js
@@ -7,3 +7,28 @@ function solution(a, b, answer = []) {
 }
 //  더 간단하게 할 수 있었음.
 //return a.reduce((acc, _, i) => acc += a[i] * b[i], 0);
+
+//약수의 개수와 덧셈
+function solution(left, right, answer = 0) {
+  let total = ((right - left + 1) * (left + right)) / 2;
+  for (let i = left; i <= right; i++) {
+    let count = [];
+    for (let j = 1; j <= Math.floor(i); j++) {
+      if (i % j === 0) count.push(j);
+    }
+    if (count.length % 2 != 0) total -= i * 2;
+  }
+  return total;
+}
+//  와.. 제곱근이 정수면, 약수의 갯수가 홀수
+//function solution(left, right) {
+//  var answer = 0;
+//  for (let i = left; i <= right; i++) {
+//    if (Number.isInteger(Math.sqrt(i))) {
+//      answer -= i;
+//    } else {
+//      answer += i;
+//    }
+//  }
+//  return answer;
+//}
