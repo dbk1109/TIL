94763ce5a429602f40006ba19c296933664f1e69
dbkim <dabin1004i@naver.com>
2023-09-04 11:25:55 +0900

    Update 직사각형 별찍기

--- 알고리즘/Programers/20230904.js
+++ 알고리즘/Programers/20230904.js
@@ -14,3 +14,14 @@ function solution(arr1, arr2) {
 function solution(arr1, arr2) {
   return arr1.map((x, i) => (x = x.map((y, j) => x[j] + arr2[i][j])));
 }
+
+//직사각형 별찍기
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  for (let i = 1; i <= b; i++) {
+    console.log("*".repeat(a));
+  }
+});
