dcb8c1e44302c428d4c94cd2ab6a8e68c647472a
dbkim <dabin1004i@naver.com>
2023-09-06 11:29:18 +0900

    Update 이상한 문자 만들기

--- 알고리즘/Programers/20230906.js
+++ 알고리즘/Programers/20230906.js
@@ -1,2 +1,5 @@
 //3진법 뒤집기
 const solution = (n) => parseInt([...n.toString(3)].reverse().join(""), 3);
+
+//이상한 문자 만들기
+const solution = s => [...s].map((x,i) => i % 2 === 0 ? x.toUpperCase() : x.toLowerCase()).join('')
