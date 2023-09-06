6318a8defb1a2147a5b2b2b97249082ac899d9f5
dbkim <dabin1004i@naver.com>
2023-09-06 11:39:01 +0900

    Update 예산

--- 알고리즘/Programers/20230906.js
+++ 알고리즘/Programers/20230906.js
@@ -3,3 +3,9 @@ const solution1 = (n) => parseInt([...n.toString(3)].reverse().join(""), 3);
 
 //이상한 문자 만들기
 const solution2 = s => s.split(" ").map(el => [...el].map((x,i) => i % 2 === 0 ? x.toUpperCase() : x.toLowerCase()).join('')).join(' ')
+
+//예산
+function solution(d, budget, count = 0) {
+    d.sort((a,b)=>a-b).forEach(x => budget >= x ? (budget -= x, count++) : budget);
+    return count;
+}
