e0ad2fc34cd50303975016ca77b47e1f3ee5d7ec
dbkim <dabin1004i@naver.com>
2023-09-18 17:57:40 +0900

    Update(hold) 푸드 파이트 대회

--- 알고리즘/Programers/20230918.js
+++ 알고리즘/Programers/20230918.js
@@ -12,3 +12,29 @@ function solution(numbers, arr = []) {
   }
   return arr.sort((a, b) => a - b).filter((x, i) => arr.indexOf(x) === i);
 }
+
+//푸드 파이트 대회
+//function solution(food) {
+//  let obj = {};
+//  var answer = [];
+//  for (let f in food) {
+//    obj[f] = food[f];
+//  }
+//  for (let i = 1; i < food.length; i++) {
+//    let arr = [];
+//    obj[i] % 2 === 0 ? arr.push(`${obj[i]}`.repeat(obj[i] / 2)) : 0;
+
+//    console.log('"', i, '" = ', obj, arr);
+//    for (let j = 0; j < food.length; j++) {
+//      console.log("> ", i, j);
+
+//      while (obj[food[j]] > 0) {
+//        arr.push(obj[food[j]]);
+//        arr.length === 2 ? answer.push(arr) : obj[food[j]]--;
+//        console.log(">> ", obj[food[j]], arr);
+//      }
+//      arr = [];
+//    }
+//  }
+//  return obj;
+//}
