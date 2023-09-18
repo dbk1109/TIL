c7a539aee734a2ef0c3b2f440dc9b9a93e70196a
dbkim <dabin1004i@naver.com>
2023-09-18 17:33:41 +0900

    Update 두 개 뽑아서 더하기

--- 알고리즘/Programers/20230918.js
+++ 알고리즘/Programers/20230918.js
@@ -2,3 +2,13 @@
 function solution(s) {
     return [...s].map((x, i) => s.indexOf(x) === i ? -1 : i - s.slice(0, i).lastIndexOf(x));
 }
+
+//두 개 뽑아서 더하기
+function solution(numbers, arr = []) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length - 1; j++) {
+      if (i != j) arr.push(numbers[i] + numbers[j]);
+    }
+  }
+  return arr.sort((a, b) => a - b).filter((x, i) => arr.indexOf(x) === i);
+}
