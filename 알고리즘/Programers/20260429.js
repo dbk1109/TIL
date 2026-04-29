f75f694d5c4e0f7db9272e885e09fffc7a14e1a3
Dabin Kim <dabin1004i@naver.com>
2026-04-30 01:48:58 +0900

    Update 모의고사

--- 알고리즘/Programers/20260429.js
+++ 알고리즘/Programers/20260429.js
@@ -1,15 +1,20 @@
 //모의고사
 function solution(answers) {
-    var answer = [];
-    let number = answers.length;
+  let P = [0, 0, 0];
+  let Winner = [];
+  const p1 = [1, 2, 3, 4, 5];
+  const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-    console.log(number);
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === p1[i % p1.length]) P[0]++;
+    if (answers[i] === p2[i % p2.length]) P[1]++;
+    if (answers[i] === p3[i % p3.length]) P[2]++;
+  }
 
-    const p1 = [1,2,3,4,5];
-    const p2 = [2,1,3,4,5];
-    const p3 = [3,1,2,4,5];
-        
-    answers.
+  for (let j = 0; j < 3; j++) {
+    if (P[j] == Math.max(...P)) Winner.push(j + 1);
+  }
 
-    return answer;
+  return Winner;
 }
